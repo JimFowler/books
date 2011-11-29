e87258186a6a4e03a3380ffe52f28039e9540a35
Jim Fowler <jrf12@mac.com>
2011-11-28 19:20:05 -0600

    Wrote contents.c and modifed Ajb contents file. Added auctions webpage.

---
+++ Books20/WebPages/Auction.php
@@ -0,0 +1,19 @@
+<?php require '/home/jrf/private_html/phpBooks.inc';?>
+<html> 
+ <head>
+ <Title>Auction Houses</Title>
+ </head>
+ <body>
+ <?php BooksTop( "Books20:<br>Auction Houses" ) ?>
+ <P>
+ <table>
+    <tr valign="Top">
+      <td width=250 height=250 valign="Top">
+      	  <?php echo LoadPrivateFile( "Auctions.txt")    ?> </td>
+    </tr>
+    </tr>
+
+
+  </table>
+ </body>
+</html>
