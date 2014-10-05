506f60fe0b5f989f7f16a8fa8db7db37e2f06772
Jim Fowler <jrf12@mac.com>
2014-10-05 17:29:33 -0500

    Added Book Binding pages

---
+++ Books20/WebPages/BookBinding.php
@@ -0,0 +1,13 @@
+<?php include 'phpBooks.inc';?>
+<html> 
+ <head>
+<Title>Book Binding</Title>
+ </head>
+ <body>
+ <?php BooksTop( "Books20:<br>Book Binding" ) ?>
+ <P>
+  <table>
+      	  <?php echo LoadPrivateFile( "BookBinding.txt")    ?>
+  </table>
+ </body>
+</html>
