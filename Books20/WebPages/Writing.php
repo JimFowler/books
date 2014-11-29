4fecab89f12fc4b9976d0f7d3fde4263dc4740f0
Jim Fowler <jrf12@mac.com>
2014-11-29 16:28:23 -0600

    Added calligraphy page and more wisdom  Changes to be committed: 	modified:   HistoryPublishing.txt 	modified:   Makefile.am 	new file:   Writing.php 	new file:   Writing.txt 	modified:   wisdom.txt

---
+++ Books20/WebPages/Writing.php
@@ -0,0 +1,13 @@
+<?php include 'phpBooks.inc';?>
+<html> 
+ <head>
+<Title>Calligraphy and Penmanship</Title>
+ </head>
+ <body>
+ <?php BooksTop( "Books20:<br>Calligraphy<br>and<br>Penmanship" ) ?>
+ <P>
+  <table>
+      	  <?php echo LoadPrivateFile( "Writing.txt")    ?>
+  </table>
+ </body>
+</html>
