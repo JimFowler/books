db64a3179822bada84cc06bcf940bd445d9c28ff
Jim Fowler <jrf12@mac.com>
2015-02-15 16:19:30 -0600

    Added Semantic Web page

---
+++ Books20/WebPages/SemanticWeb.php
@@ -0,0 +1,13 @@
+<?php include 'phpBooks.inc';?>
+<html> 
+ <head>
+<Title>The Semantic Web</Title>
+ </head>
+ <body>
+ <?php BooksTop( "Books20:<br>Semantic Web" ) ?>
+ <P>
+  <table>
+      	  <?php echo LoadPrivateFile( "SemanticWeb.txt")    ?>
+  </table>
+ </body>
+</html>
