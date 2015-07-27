6ed6103d5316580d2b4bec8dabe0d9516c937a55
Jim Fowler <jrf12@mac.com>
2015-07-26 19:01:19 -0500

    wrote ppxml program
     Changes to be committed:
    	modified:   ../ChangeLog
    	new file:   ppxml
    	modified:   setup.py

--- Books20/Tools/setup.py
+++ Books20/Tools/setup.py
@@ -1,14 +1,24 @@
+#
+# setup.py
+#
+# installation instructions for python install
+#
+# run as
+#   python3 setup.py install --prefix=/home/jrf
+#
 from distutils.core import setup
 
-setup( name='BookEntry',
+setup( name='bookentry',
        version='1.0.0',
        description='AJB BooksEntry tool',
        author='James R. Fowler',
        author_email='jrf12@mac.com',
        url='http://het.as.utexas.edu/jrf/20thCentury.html',
-       platforms=['Ubuntu 12.04'],
+       platforms=['Ubuntu 12.04',
+                  'Ubuntu 13',
+                  'Ubuntu 14.10 LTS'],
        packages=['bookentry'],
-       scripts=['ajbbooks'],
+       scripts=['ajbbooks', 'ppxml'],
        package_data={'bookentry': ['symbols.txt']},
        )
 
