3f5781dcf0d54f72d880d17c62d2b73ba92034fa
Jim Fowler <jrf12@mac.com>
2017-08-19 16:00:11 -0500

    Removed 10 line limit
    We now show all matching entries and allow scrollbars.
    Also made search dialog box a little smaller.
    
    Cleaned up journalsearch.py to satisfy pylint
    
    On branch master
    
     Changes to be committed:
    	modified:   Books20/Tools/bookentry/designer/ui_JournalSearch.ui
    	modified:   Books20/Tools/bookentry/journalsearch.py

--- Books20/Tools/bookentry/journalsearch.py
+++ Books20/Tools/bookentry/journalsearch.py
@@ -1,96 +1,74 @@
 #! /usr/bin/env python3
 # -*- mode: Python;-*-
-
-"""Dialog window for journal entries from AAA/AJB
+'''Dialog window for journal entries from AAA/AJB
 
   open with Journal Search
   open existing journalFile or new one
   New button or search brings up Entry window with an already open file
   redoing a search sets the index number in the current entry
   Search by full name, abbreviation, closeness (how to do this)
-"""
-import sys
-import os
-import platform
-import fileinput
-import configparser
-
-from PyQt5.QtCore import *
-from PyQt5.QtGui  import *
-from PyQt5.QtWidgets import *
+'''
+from PyQt5 import QtWidgets
 
 import bookentry.ui_JournalSearch as ui_journalSearch
-import bookentry.search as search
 
-# Trouble shooting assistance
-from pprint import pprint
+class JournalSearch(QtWidgets.QDialog, ui_journalSearch.Ui_JournalSearch):
+    '''The main class to create a widget and allow searchs.'''
 
-__version__ = '1.0.0'
-
-class JournalSearch( QDialog, ui_journalSearch.Ui_JournalSearch ):
-
-    def __init__(self, parent = None, searchDict = None ):
+    def __init__(self, parent=None, searchDict=None):
         super(JournalSearch, self).__init__(parent=parent)
         self.setupUi(self)
 
         self.searchdict = searchDict
         self.parent = parent
-        
-        self.searchEdit.textChanged.connect(self.SearchEditChanged)
-        self.searchResults.itemDoubleClicked.connect(self.titleSelected)
-      
+        self.current_search_list = []
+
+        self.searchEdit.textChanged.connect(self.search_edit_changed)
+        self.searchResults.itemDoubleClicked.connect(self.title_selected)
+
 
         self.closeButton.released.connect(self.close)
         self.newButton.released.connect(self.parent.newEntry)
-  
+
 
     #
     # This function is the slot for the searchEdit.textChanged signal
     #
-    def SearchEditChanged(self):
+    def search_edit_changed(self):
         """Run a search of the sdict and fill in the searchResults"""
-        #print('SearchEditChanged')
-        try:
-            self.searchEdit.textChanged.disconnect(self.SearchEditChanged)
-        except:
-            pass
+        self.searchEdit.textChanged.disconnect(self.search_edit_changed)
 
         search_text = self.searchEdit.text()
         self.searchResults.clear()
         self.searchResults.insertItem(0, search_text.strip())
 
-        if len(search_text) > 2: 
+        if len(search_text) > 2:
             # Update the searchResults box
             try:
-                self.currentSearchList = self.searchdict.search(search_text.strip())
+                self.current_search_list = self.searchdict.search(search_text.strip())
             except KeyError:
-                print('no entry in search dict')
-                self.currentSearchList = []
-                
-            if len(self.currentSearchList) > 0:
+                self.current_search_list = []
+
+            max_items = len(self.current_search_list)
+            if max_items > 0:
                 # add to searchResults
-                for i in range(1, min(9,len(self.currentSearchList)+1)):
+                for i in range(1, (max_items + 1)):
                     try:
-                        self.searchResults.insertItem(i, self.currentSearchList[i-1][0])
+                        self.searchResults.insertItem(i, self.current_search_list[i-1][0])
                     except IndexError:
                         pass
-            
-        try:
-            self.searchEdit.textChanged.connect(self.SearchEditChanged)
-        except:
-            pass
-     
-    def titleSelected(self, title):
-        """If a title has been selected in the searchResults, then
-        find that title in the currentSearchList, extract that record
-        number and show the entry with the relevant record number."""
-        tt = title.text()
-        for t, i in self.currentSearchList:
-            if t == tt:
-                #print(tt, t, i)
-                self.parent.showEntry(i+1)
-                return
-            
-        self.parent.newEntry()
-        self.parent.titleEdit.setText(tt)
 
+        self.searchEdit.textChanged.connect(self.search_edit_changed)
+
+    def title_selected(self, search_title):
+        """If a title has been selected in the searchResults, then
+        find that title in the current_search_list, extract that record
+        number and show the entry with the relevant record number."""
+        title_text = search_title.text()
+        for title, index in self.current_search_list:
+            if title == title_text:
+                self.parent.showEntry(index + 1)
+                return
+
+        self.parent.newEntry()
+        self.parent.titleEdit.setText(title_text)
