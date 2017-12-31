796327bbe4646b551f37a9809b2c6127d959cfce
Jim Fowler <jrf12@mac.com>
2017-12-30 19:43:16 -0600

    Started Digital Humanities web page
    On branch master
    
     Changes to be committed:
    	new file:   Books20/WebPages/DigitalHum.php
    	new file:   Books20/WebPages/DigitalHum.txt
    	modified:   Books20/WebPages/Makefile.am

---
+++ Books20/WebPages/DigitalHum.php
@@ -0,0 +1,13 @@
+<?php include 'phpBooks.inc';?>
+<html> 
+ <head>
+<Title>Digital Humanties</Title>
+ </head>
+ <body>
+ <?php BooksTop( "Books20:<br>Digital Humanities" ) ?>
+ <P>
+  <table>
+      	  <?php echo LoadPrivateFile( "DigitalHum.txt")    ?>
+  </table>
+ </body>
+</html>
