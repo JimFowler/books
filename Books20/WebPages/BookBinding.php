1fccb62b456a8e0983da9df0e32be976a2ce4182
Jim Fowler <jrf12@mac.com>
2018-09-09 13:14:23 -0500

    Added standard copyright to webpages
     On branch master
    
     Changes to be committed:
    	modified:   Books20/WebPages/20thCentury.php
    	modified:   Books20/WebPages/Auction.php
    	modified:   Books20/WebPages/Bibliographies.php
    	modified:   Books20/WebPages/BlogLists.php
    	modified:   Books20/WebPages/BookBinding.php
    	modified:   Books20/WebPages/BooksBooks.php
    	modified:   Books20/WebPages/DigitalHum.php
    	modified:   Books20/WebPages/SemanticWeb.php
    	modified:   Books20/WebPages/ToDoList.php
    	modified:   Books20/WebPages/Writing.php
    	modified:   Books20/WebPages/index.html

--- Books20/WebPages/BookBinding.php
+++ Books20/WebPages/BookBinding.php
@@ -1,4 +1,22 @@
 <?php include 'phpBooks.inc';?>
+<!-- Begin Copyright
+
+  /home/jrf/Documents/books/Books20/WebPages/BookBinding.php
+  
+   Part of the Books20 Project
+
+   Copyright 2018 James R. Fowler
+
+   All rights reserved. No part of this publication may be
+   reproduced, stored in a retrival system, or transmitted
+   in any form or by any means, electronic, mechanical,
+   photocopying, recording, or otherwise, without prior written
+   permission of the author.
+
+
+End Copyright -->
+
+
 <html> 
  <head>
 <Title>Book Binding</Title>
