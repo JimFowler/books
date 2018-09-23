14b9df594dfa10afd31c999345531d0831235783
Jim Fowler <jrf12@mac.com>
2018-09-23 13:47:04 -0500

    Added Changelog entry
    And improve PEP-8 compliance for setup.py
    
     On branch master
    
     Changes to be committed:
    	modified:   Books20/ChangeLog
    	modified:   Books20/Tools/python/setup.py

--- Books20/Tools/python/setup.py
+++ Books20/Tools/python/setup.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 # -*- mode: Python;-*-
-# -*- coding: UTF-8 -*-
 ## Begin copyright
 ##
 ##  /home/jrf/Documents/books/Books20/Tools/python/setup.py
-##  
+##
 ##   Part of the Books20 Project
 ##
 ##   Copyright 2018 James R. Fowler
@@ -23,19 +22,17 @@
 # run as
 #   python3 setup.py install --prefix=/home/jrf
 #
-
+'''Installation for python tools in Books20'''
 
 from distutils.core import setup
 
-setup( name='bookentry',
-       version='1.0.0',
-       description='AJB BooksEntry tool',
-       author='James R. Fowler',
-       author_email='jrf12@mac.com',
-       url='http://het.as.utexas.edu/jrf/20thCentury.html',
-       platforms=['Ubuntu 18.04 LTS'],
-       packages=['bookentry'],
-       scripts=['ajbbooks', 'journals', 'ppxml', 'validate_xml'],
-       package_data={'bookentry': ['symbols.txt']},
-       )
-
+setup(name='bookentry',
+      version='1.0.0',
+      description='AJB BooksEntry tool',
+      author='James R. Fowler',
+      author_email='jrf12@mac.com',
+      url='http://het.as.utexas.edu/jrf/20thCentury.html',
+      platforms=['Ubuntu 18.04 LTS'],
+      packages=['bookentry'],
+      scripts=['ajbbooks', 'journals', 'ppxml', 'validate_xml'],
+      package_data={'bookentry': ['symbols.txt']},)
