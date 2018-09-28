1adcbb991dde5c2d77c211a9e3686ffb31bfb64f
Jim Fowler <jrf12@mac.com>
2018-09-27 19:46:51 -0500

    Made entryselect.py compliant with PEP-8
    Need to make it more generic
    
     On branch aabooks
     Changes to be committed:
    	modified:   Books20/Tools/python/aabooks/lib/entryselect.py

--- Books20/Tools/python/aabooks/lib/entryselect.py
+++ Books20/Tools/python/aabooks/lib/entryselect.py
@@ -1,7 +1,7 @@
 ## Begin copyright
 ##
 ##  /home/jrf/Documents/books/Books20/Tools/python/aabooks/lib/entryselect.py
-##  
+##
 ##   Part of the Books20 Project
 ##
 ##   Copyright 2018 James R. Fowler
@@ -18,85 +18,94 @@
 
 """Choose an entry from the list."""
 
+from PyQt5 import QtCore, QtGui, QtWidgets
 
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
+import aabooks.lib.ui_EntrySelect as EntrySelect_ui
 
-import aabooks.lib.ui_EntrySelect as ui_EntrySelect
+class EntrySelect(QtWidgets.QDialog, EntrySelect_ui.Ui_ShortTitleDisplay):
+    '''Provides a dialog box from a list.  Selecting an item by
+    double clicking or highlighting and selecting Ok will cause
+    the item to be emitted as a signal.
 
-class EntrySelect(QDialog, ui_EntrySelect.Ui_ShortTitleDisplay):
+    '''
 
-    lineEmit = pyqtSignal( object, name='lineEmit')
+    lineEmit = QtCore.pyqtSignal(object, name='lineEmit')
 
-    def __init__(self, parent=None, shortTitleList=None):
+    def __init__(self, entry_list=None):
         super(EntrySelect, self).__init__()
 
         self.setupUi(self)
 
-        
-        self.redBkgd = QTextCharFormat()
-        self.redBkgd.setBackground(QBrush(QColor("red")))
+        self.red_bkgd = QtGui.QTextCharFormat()
+        self.red_bkgd.setBackground(QtGui.QBrush(QtGui.QColor("red")))
 
-        self.whiteBkgd = QTextCharFormat()
-        self.whiteBkgd.setBackground(QBrush(QColor("white")))
+        self.white_bkgd = QtGui.QTextCharFormat()
+        self.white_bkgd.setBackground(QtGui.QBrush(QtGui.QColor("white")))
         self.first = True
-        if shortTitleList is not None:
-            self.setText(shortTitleList)
+        if entry_list is not None:
+            self.set_text(entry_list)
 
-        self.okButton.released.connect(self.returnEntry)
-        self.cancelButton.released.connect(self.returnNone)
+        self.okButton.released.connect(self.return_entry)
+        self.cancelButton.released.connect(self.return_none)
         self.textWindow.cursorPositionChanged.connect(self.highlight)
-        self.textWindow.mouseDoubleClickEvent = self.mouseDoubleClick
-        
-    def mouseDoubleClick(self, mouseEvent):
+        self.textWindow.mouseDoubleClickEvent = self.mouse_double_click
+
+    def mouse_double_click(self, mouse_event):
         """Override the textBrowser's mouseDoubleClickEvent() function
         so we can choose the entry by double clicking."""
-        self.returnEntry()
+        #if mouse_event:
+        self.return_entry()
 
-    def setText(self, text):
+    def set_text(self, text):
+        '''set text'''
         self.textWindow.setText(text)
         self.cursor = self.textWindow.textCursor()
-        self.cursor.movePosition(QTextCursor.StartOfLine, QTextCursor.MoveAnchor)
-        self.cursor.movePosition(QTextCursor.EndOfLine, QTextCursor.KeepAnchor)
-        self.cursor.mergeCharFormat(self.whiteBkgd)
+        self.cursor.movePosition(QtGui.QTextCursor.StartOfLine,\
+                                 QtGui.QTextCursor.MoveAnchor)
+        self.cursor.movePosition(QtGui.QTextCursor.EndOfLine,
+                                 QtGui.QTextCursor.KeepAnchor)
+        self.cursor.mergeCharFormat(self.white_bkgd)
         self.first = True
 
-    def returnNone(self):
+    def return_none(self):
+        ''' return none'''
         self.close()
 
-    def returnEntry(self):
+    def return_entry(self):
+        ''' return Entry'''
         # emit selection and close?
         #print('selected: %s '% self.cursor.selectedText())
-        emitStr = self.cursor.selectedText()
-        self.lineEmit.emit( (emitStr, ) )
+        emit_string = self.cursor.selectedText()
+        self.lineEmit.emit((emit_string, ))
         self.close()
 
     def highlight(self):
+        '''highlight'''
         if not self.first:
-            self.cursor.mergeCharFormat(self.whiteBkgd)
+            self.cursor.mergeCharFormat(self.white_bkgd)
         self.first = False
         self.cursor = self.textWindow.textCursor()
-        self.cursor.movePosition(QTextCursor.StartOfLine, QTextCursor.MoveAnchor)
-        self.cursor.movePosition(QTextCursor.EndOfLine, QTextCursor.KeepAnchor)
-        self.cursor.mergeCharFormat(self.redBkgd)
+        self.cursor.movePosition(QtGui.QTextCursor.StartOfLine,
+                                 QtGui.QTextCursor.MoveAnchor)
+        self.cursor.movePosition(QtGui.QTextCursor.EndOfLine,
+                                 QtGui.QTextCursor.KeepAnchor)
+        self.cursor.mergeCharFormat(self.red_bkgd)
 
 
 if __name__ == '__main__':
 
-    import sys
+    def print_me(_me):
+        ''' print me '''
+        print('You selected line:', _me[0])
 
-    def printMe(l):
-        print('You selected line:', l[0])
-        
-    app = QApplication(sys.argv)
-    ex = EntrySelect()
-    ex.show()
-    txtStr = """1 AJB 55.06.07 Author1, Title1
+    APP = QtWidgets.QApplication(sys.argv)
+    EX = EntrySelect()
+    EX.show()
+    TXTSTR = """1 AJB 55.06.07 Author1, Title1
 2 AJB 55.06.08 Author2, Title2
 3 AJB 55.06.09 Author3, Title3
 """
-    ex.setText(txtStr)
-    ex.lineEmit.connect(printMe)
+    EX.set_text(TXTSTR)
+    EX.lineEmit.connect(print_me)
 
-    app.exec_()
+    APP.exec_()
