8a60021d7de4f36a407116963e4e44a8f6d2f466
Jim Fowler <jrf12@mac.com>
2018-09-27 19:45:54 -0500

    Change path from bookentry to aabooks
     On branch aabooks
     Changes to be committed:
    	modified:   Books20/Tools/python/doc/ajbbooks/conf.py
    	modified:   Books20/Tools/python/doc/journals/conf.py

--- Books20/Tools/python/doc/ajbbooks/conf.py
+++ Books20/Tools/python/doc/ajbbooks/conf.py
@@ -20,8 +20,7 @@ import sys, os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../..'))
-sys.path.insert(0, os.path.abspath('../../bookentry'))
-sys.path.insert(0, os.path.abspath('../../bookentry/xml'))
+sys.path.insert(0, os.path.abspath('../../aabooks/ajbbook'))
 
 # -- General configuration -----------------------------------------------------
 
