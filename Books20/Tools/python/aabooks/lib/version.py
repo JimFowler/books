d6b774fa8cda2bc48ccf5ea2fcd161712a1de347
Jim Fowler <jrf12@mac.com>
2018-11-25 13:45:30 -0600

    Clean up version.py
     On branch aabooks
     Changes to be committed:
    	modified:   Books20/Tools/python/aabooks/lib/version.py

--- Books20/Tools/python/aabooks/lib/version.py
+++ Books20/Tools/python/aabooks/lib/version.py
@@ -3,7 +3,7 @@
 ## Begin copyright
 ##
 ##  /home/jrf/Documents/books/Books20/Tools/python/aabooks/lib/version.py
-##  
+##
 ##   Part of the Books20 Project
 ##
 ##   Copyright 2018 James R. Fowler
@@ -31,5 +31,3 @@ __minor__ = __version_info__[1]
 __micro__ = __version_info__[2]
 
 __version__ = '.'.join(__version_info__)
-
-
