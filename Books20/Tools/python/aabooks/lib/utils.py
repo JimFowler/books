054840f8b60ca5a9e0b5e95fb55fd9868d451370
Jim Fowler <jrf12@mac.com>
2018-11-25 13:45:56 -0600

    Clean up utils.py
     On branch aabooks
     Changes to be committed:
    	modified:   Books20/Tools/python/aabooks/lib/utils.py

--- Books20/Tools/python/aabooks/lib/utils.py
+++ Books20/Tools/python/aabooks/lib/utils.py
@@ -1,7 +1,7 @@
 ## Begin copyright
 ##
 ##  /home/jrf/Documents/books/Books20/Tools/python/aabooks/lib/utils.py
-##  
+##
 ##   Part of the Books20 Project
 ##
 ##   Copyright 2018 James R. Fowler
@@ -14,7 +14,6 @@
 ##
 ##
 ## End copyright
-
 '''Utility functions that may be used in various programs.'''
 
 from nameparser import HumanName
@@ -23,38 +22,36 @@ from nameparser.config import CONSTANTS
 # add SJ Suffix list
 CONSTANTS.suffixes.add('sj')
 
-def makeNameList( line, sep=' and ' ) :
+def make_name_list(line, sep=' and '):
     """Returns a list of object of class HumanName. See the package
     nameparser for full info. The names have the following possible keys
     "Title", "First", "Middle", "Last", and "Suffix"
     """
     name_list = []
     names = line.split(sep)
-    
-    for name in names :
-        nm = HumanName( name.strip() )
-        name_list.append(nm)
+
+    for name in names:
+        clean_name = HumanName(name.strip())
+        name_list.append(clean_name)
 
     return name_list
 
-def makeNameStr( namelist, sep=' and '):
+def make_name_str(name_list, sep=' and '):
     """Returns a string built from a list of HumanName objects.
     See the package nameparser for details about HumanName.
     """
+    name_str = ''
+    if not name_list:
+        return name_str
 
-    nameStr = ''
-    if not namelist:
-        return nameStr
-    
     first = True
-    for nm in namelist:
+    for name in name_list:
         if not first:
-            nameStr += sep
+            name_str += sep
         first = False
-        nameStr += str(nm)
-        
-    return nameStr
+        name_str += str(name)
+
+    return name_str
 
 if __name__ == '__main__':
     print('No tests available yet')
-    
