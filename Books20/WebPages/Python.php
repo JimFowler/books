b1c23d4680750244f5922e82ad0ff6bc4b59cdc2
Jim Fowler <jrf12@mac.com>
2021-06-01 21:25:08 -0500

    Added a web page detailing the python installation
     On branch master
    
     Changes to be committed:
    	modified:   Books20/WebPages/Makefile.am
    	new file:   Books20/WebPages/Python.php
    	new file:   Books20/WebPages/PythonTools.txt
    	modified:   Books20/WebPages/Tools.txt

---
+++ Books20/WebPages/Python.php
@@ -0,0 +1,31 @@
+<?php include 'phpBooks.inc';?>
+ <!-- Begin Copyright
+
+  /home/jrf/Documents/books/Books20/WebPages/Python.php
+  
+   Part of the Books20 Project
+
+   Copyright 2021 James R. Fowler
+
+   All rights reserved. No part of this publication may be
+   reproduced, stored in a retrival system, or transmitted
+   in any form or by any means, electronic, mechanical,
+   photocopying, recording, or otherwise, without prior written
+   permission of the author.
+
+
+ End Copyright -->
+
+
+<html> 
+ <head>
+<Title>Python Tools</Title>
+ </head>
+ <body>
+ <?php BooksTop( "Books20:<br>Python Tools" ) ?>
+ <P>
+  <table>
+      	  <?php echo LoadPrivateFile( "PythonTools.txt")    ?>
+  </table>
+ </body>
+</html>
