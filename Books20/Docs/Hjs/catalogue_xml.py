ee3c3d914bce2e21533ace0adad6f75763057047
Jim Fowler <jrf12@mac.com>
2023-01-13 12:16:19 -0600

    Created class for HJS catalogue entries
    Modified catalogue_xml.py to take advantage of
    the new class.
    
     On branch master
    
     Changes to be committed:
    	deleted:    Books20/Docs/Hjs/author2.idx
    	modified:   Books20/Docs/Hjs/catalogue_xml.py
    	new file:   Books20/Docs/Hjs/hjsentry.py

--- Books20/Docs/Hjs/catalogue_xml.py
+++ Books20/Docs/Hjs/catalogue_xml.py
@@ -19,50 +19,17 @@
 '''Make a catalogue from an XML bookfile.  Choose what order to list
 the books.  Create a LaTeX file for processing. Catalogue should look like
 
-Intro
-  Who, what, where, when, how
-
-
-TODO:
-
-  really need to make this a class that takes an entry
-
-  pick better font for catalogue entries
-
-  DONE identify bookplates in hjs01_books.xml
-  DONE indent year place publisher??
-  DONE use \vbox for both entry and comments to prevent page breaks in comments
-  DONE add sensible pagination, do not use my codes for page counts
-  DONE use Roman numerals for preface page counts
-  DONE determine proper order of comments
-   edition
-   series
-   books notes
-    laid in, etc.
-    dedication
-   dust jacket
-   binding
-   bookplate
-   library stamp
-   signature/initials
-   ISBN
-  DONE remove "ownership" words
-  DONE print sub-titles
-  DONE change 'selbtsverlog' to 'self-published'
-  DONE change ' "' to '``' to get TeX quote marks
-  DONE add additional space between year and place ?? or add comma
-
 '''
 import sys
 from pprint import pprint
 
-from aabooks.ajbbook import bookfile as bf
-from aabooks.lib import utils as aautils
-from aabooks.lib import roman
-
 import configargparse as argp
-
 from nameparser.config import CONSTANTS
+
+from aabooks.lib import utils as aautils
+from aabooks.ajbbook import bookfile as bf
+import hjsentry
+
 CONSTANTS.initials_format='{first}{middle}'
 
 __VERSION__ = '2.0'
@@ -97,14 +64,6 @@ def get_args():
     return parser.parse_known_args()[0]
 
 
-def protect(raw_str):
-    '''Protect the LaTeX special characters that we want to print.
-
-    '''
-
-    new_str = raw_str.replace(r"&", r'\&')
-    return new_str
-
 def sort_bookfile(bookfile, sort_flag):
     '''Use the sort flag to determine how to sort the bookfile.
 
@@ -115,288 +74,6 @@ def sort_bookfile(bookfile, sort_flag):
         print(f'sorting bookfile by {sort_flag}')
         bookfile.sort(sort_name=sort_flag)
 
-def print_header(outf=sys.stdout):
-    '''Print the leading material for the catalogue.
-
-    '''
-    header = protect(r'\newcounter{bksctr}')
-    print(header, file=outf)
-
-def make_name_string(hname, name_style='first'):
-    '''Make the name string in the desired style from a HumanName
-    object. There are two forms of each style determine by the
-    presence of a first name or a first initial.  The different is the
-    ~ tie for LaTeX.
-
-    first -- Last, First MI.
-    first -- Last, FI.~MI.
-
-    second -- First MI., Last
-    second -- FI.~MI., Last
-
-    '''
-
-    initials = hname.initials()
-
-    if 'first' in name_style:
-        name = f'{hname.last}'
-        if initials:
-            name += f', {initials}'
-    elif 'second' in name_style:
-        name = f'{initials} {hname.last}'
-    else:
-        name = r''
-
-    return name.strip()
-
-
-def get_author_string(entry):
-    '''Create an author/editor string from an entry.
-
-    '''
-
-    authors = entry['Authors']
-    editors = entry['Editors']
-    ret_str = ''
-
-    if len(authors):
-        # first author
-        ret_str = r'''\textit{'''
-        ret_str += make_name_string(authors[0], name_style='first')
-        ret_str += r'}'
-        if len(authors) > 2:
-            ret_str += r', et.\,al.'
-        elif len(authors) == 2:
-            ret_str += r' and \textit{'
-            ret_str += make_name_string(authors[1], name_style='second')
-            ret_str += '}'
-    elif len(editors):
-        # first editor
-        ret_str = r'''\textit{'''
-        ret_str += make_name_string(editors[0])
-        ret_str += r'}'
-        if len(editors) > 2:
-            ret_str += r', et.\,al., eds.'
-        elif len(editors) == 2:
-            ret_str += r' and \textit{'
-            ret_str += make_name_string(editors[1], name_style='second')
-            ret_str += r'}, eds.'
-        else:
-            ret_str += ', ed.'
-
-    if ret_str:
-        # Only add this comma if the name string is not empty
-        # otherwise the title is preceeded by a single comma
-        ret_str += ','
-
-    return ret_str
-
-class Comma():
-    '''Add a comma and a space unless first is True'''
-    def __init__(self):
-        self.first_comma = True
-
-    def test(self):
-        '''Test the value of first_comma and return the
-        appropriate string.
-
-        '''
-        if self.first_comma:
-            self.first_comma = False
-            return r'\hspace{1em}'
-
-        return ', '
-
-    def isfirst(self):
-        '''Return the value of self.first_comma'''
-        return self.first_comma
-
-def make_pagination_str(pagination):
-    '''Create a clean and neat pagination string given a string
-    of the form 12pp+235p+12P.
-
-    '''
-
-    if not pagination:
-        return ''
-
-    pages = ''
-    page_counts = pagination.split('+')
-    comma = Comma()
-
-    for page in page_counts:
-        if 'pp' in page:
-            pg_str = int(page.split(r'pp')[0])
-            pages += comma.test() + roman.to_roman(pg_str).lower()
-        elif 'p' in page:
-            pg_str = page.split('p')[0]
-            pages += comma.test() + str(pg_str) + r'\,pp'
-        elif 'P' in page:
-            pg_str = page.split('P')[0]
-            pages += comma.test() + str(pg_str) + r'\,plates'
-        elif 'c' in page:
-            pg_str = page.split('c')[0]
-            pages += comma.test() + str(pg_str) + r'\,charts'
-        elif 'I' in page:
-            pg_str = page.split('I')[0]
-            pages += comma.test() + str(pg_str) + r'p index'
-        elif 'AA' in page:
-            pg_str = page.split('AA')[0]
-            pages += comma.test() + str(pg_str) + r'p app.\ A'
-        elif 'AB' in page:
-            pg_str = page.split('AB')[0]
-            pages += comma.test() + str(pg_str) + r'p app.\ B'
-        elif 'AC' in page:
-            pg_str = page.split('AC')[0]
-            pages += comma.test() + str(pg_str) + r'p app.\ C'
-        elif page:
-            print(page)
-        else:
-            pass
-
-    return pages
-
-
-def make_edition(edition):
-    '''Create a proper string for the edition number.
-    i.e. 1^{st}, 2^{nd}, etc. This should be able to handle
-    any edition number. edition may be a string, int, or float.
-    '''
-
-    ed_num = int(edition)
-    last_digit = str(ed_num)[-1]
-    #if 11 == ed_num or 12 == ed_num or 13 == ed_num:
-    if ed_num in [11, 12, 13]:
-        suffix = 'th'
-    elif '1' in last_digit:
-        suffix = 'st'
-    elif '2' in last_digit:
-        suffix = 'nd'
-    elif '3' in last_digit:
-        suffix = 'rd'
-    else:
-        suffix = 'th'
-
-
-    return r'\Ord{' + str(edition) + '}{' + suffix + '} ' + 'edition'
-
-def clean_comment(comment):
-    '''Clean up my parsable comments in the XML files
-    with good LateX comments.
-
-    These steps will missing quotes at the begining or end
-    of a line or followed by a period or comma. Should use a regex.
-
-    '''
-    cleaned = comment.replace(' "', ' ``')
-    cleaned = cleaned.replace('" ', "'' ")
-
-    return cleaned
-
-def print_entry(count, entry, authidx, outf=sys.stdout):
-    '''Print an entry as number 'count' for the catalogue. These are
-    expected to be of the form AJBEntry.
-
-    '''
-
-    tex_entry = protect(r'''\stepcounter{bksctr}
-\vbox{%
-  \vspace*{0.5 cm}
-  \noindent
-''')
-
-    # add reference label
-    # make the Author, Title line
-    author = get_author_string(entry)
-
-    title = entry['Title']
-
-    tex_entry += r'''  \hypertarget{entry:''' + str(count)
-    tex_entry += r'''}{\footnotesize\arabic{bksctr}} ''' + author
-    tex_entry += r''' \textsc{\bfseries ''' + title + r'''}'''
-
-    print(protect(tex_entry), file=outf)
-    print(file=outf)
-
-    # Create author index entries here
-    if entry['Authors']:
-        for author in entry['Authors']:
-            output = r'\indexentry{' + make_name_string(author)
-            output += r'|myhref}{' + f'{count}' + r'}'
-            print(output, file=authidx)
-    if entry['Editors']:
-        for author in entry['Editors']:
-            output = r'\indexentry{' + make_name_string(author)
-            output += r'|myhref}{' f'{count}' + r'}'
-            print(output, file=authidx)
-    # make the Year, Place, Publisher line
-    # Get only the first publisher, if there is one listed.
-    try:
-        place = entry['Publishers'][0]['Place'].split('-')[0]
-        if place:
-            place += ','
-    except IndexError:
-        place = ''
-
-    try:
-        publisher = entry['Publishers'][0]['PublisherName']
-        publisher = publisher.replace('selbstverlag', 'self-published')
-    except IndexError:
-        publisher = ''
-
-    try:
-        year = str(entry['Year'])
-    except ValueError:
-        year = ''
-
-    try:
-        pagination = entry['Pagination']
-        pages = make_pagination_str(pagination)
-    except KeyError:
-        pages = ''
-
-    if year or place or publisher or pages:
-        if not year:
-            year=r'\hspace{3em}'
-        else:
-            year += r'\hspace{0.5em}'
-        if not place:
-            place = r'\hspace{5em}'
-        if publisher and pages:
-            page_comma = ','
-        else:
-            page_comma = ''
-
-        year_pub_entry = ' '.join([year, place, publisher]) + page_comma + pages
-
-        print(protect(year_pub_entry), file=outf)
-        print(file=outf)
-
-    try:
-        edition = entry['Edition']
-        if edition:
-            print(protect(make_edition(edition)), file=outf)
-            print(file=outf)
-    except KeyError:
-        pass
-
-    for comment in entry['Others']:
-        clean_com = clean_comment(comment)
-        print(protect(clean_com), file=outf)
-        print(file=outf)
-
-    print(entry.num_str(), file=outf)
-    print(r'''}
-
-''', file=outf)
-
-def print_closing(outf=sys.stdout):
-    '''Print any closing material for the catalogue.
-
-    '''
-    print('Printing the closing material for the catalogue', file=outf)
-
-
 def main():
     '''Do all the work here
 
@@ -404,6 +81,8 @@ def main():
     args = get_args()
     if args.verbose:
         pprint(args)
+
+    hjs_entry = hjsentry.HjsEntry()
 
     with open(args.output, 'w', encoding = 'UTF8') as filep:
         authp = open('author2.idx', 'w', encoding = 'UTF8')
@@ -414,15 +93,15 @@ def main():
         if args.sort:
             bookf.sort_by(args.sort)
 
-        print_header(outf=filep)
+        hjs_entry.print_header(outf=filep)
         for count, ent in enumerate(bookf, start=1):
             try:
-                print_entry(count, ent, authp, outf=filep)
+                hjs_entry.print_entry(count, ent, authp, outf=filep)
             except [KeyError, ValueError] as exp:
                 pprint(exp)
                 print('problem with entry:', count + 1)
                 pprint(ent)
-        print_closing()
+        hjs_entry.print_closing()
 
     filep.close()
 
