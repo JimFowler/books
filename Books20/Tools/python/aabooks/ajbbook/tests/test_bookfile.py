f471f1e60275e3081eb7071a9956d42d5ff16f2f
Jim Fowler <jrf12@mac.com>
2024-06-16 19:38:34 -0500

    Got all bookfile unittests working
     On branch master
    
     Changes to be committed:
    	modified:   Books20/Tools/python/aabooks/ajbbook/tests/test_bookfile.py

--- Books20/Tools/python/aabooks/ajbbook/tests/test_bookfile.py
+++ Books20/Tools/python/aabooks/ajbbook/tests/test_bookfile.py
@@ -20,6 +20,7 @@
 import unittest
 from lxml import etree
 from aabooks.ajbbook import bookfile as bf
+from aabooks.ajbbook import __sort_dict__
 
 class BookFileFunctionTestCase(unittest.TestCase):
     '''Tests for the class BookFile.'''
@@ -51,22 +52,11 @@ class BookFileFunctionTestCase(unittest.TestCase):
         shorttitle = self.bookfile.make_short_title_list()
         self.assertEqual(len(shorttitle), 19742)
 
-    def test_d_read_file_text(self):
-        '''Test the BookFile.read_file_text() method.'''
-
-        self.assertEqual(self.bookfile.read_file("testfile.txt"), 11)
-
-    def test_d_write_file_text(self):
-        '''Test the BookFile.write_file_text() method.'''
-
-        self.assertEqual(self.bookfile.read_file("testfile.txt"), 11)
-        self.assertTrue(self.bookfile.write_file('testfile_tmp.txt'))
-
-    def test_f_check_schema(self):
+    def test_d_check_schema(self):
         '''Check the schema that defines a book file and check the file
         that we wrote when we tested write_file_xml().'''
+        bf_schema = etree.XMLSchema(file='../../../../xml/bookfile.xsd')
         try:
-            bf_schema = etree.XMLSchema(file='../../../xml/bookfile.xsd')
             parser = etree.XMLParser(schema=bf_schema)
             schema_good = True
         except etree.XMLSchemaParseError:
@@ -83,7 +73,7 @@ class BookFileFunctionTestCase(unittest.TestCase):
         self.assertTrue(xml_file_good,
                         msg='The xml file is not well formed or is invalid')
 
-    def test_g_add_bookfiles(self):
+    def test_e_add_bookfiles(self):
         '''Test the __add__() or '+' function in order to see
         what is happening. Not much of a test as it simply duplicates
         our first implementation of __add__().
@@ -106,7 +96,7 @@ class BookFileFunctionTestCase(unittest.TestCase):
         del test_file
         del answer_file
 
-    def test_j_extend_list(self):
+    def test_f_extend_list(self):
         '''Test the __extend__() function in order to see
         what is happening. Not much of a test as it simply duplicates
         the first implementation of __add__().
@@ -143,21 +133,30 @@ class BookFileSortTestCase(unittest.TestCase):
         del self.bookfile
 
     def is_sorted(self, key):
-        '''Test of the bookfile is sorted by the given key. '''
+        '''Test if the bookfile is sorted by the given key. '''
 
+        lower_key = __sort_dict__[key]
         first = ''
         for e in self.bookfile:
-            if 'num' in key.lower():
+            if 'num' in lower_key:
                 second = e.sort_num_str()
+            elif 'author' in lower_key:
+                if e['Authors']:
+                    second = e['Authors'][0].last.lower()
+                elif e['Editors']:
+                    second = e['Editors'][0].last.lower()
+                else:
+                    second = ''
             else:
                 second = e[key]
             if second < first:
+                #print(f'author: {first = }  {second = }')
                 return False
             first = second
         return True
 
     def test_a_sort_by_year(self):
-        '''Test the sort_by routine in ajbentry.py.
+        '''Test the sort_by routine with Year in ajbentry.py.
 
         '''
 
@@ -166,25 +165,18 @@ class BookFileSortTestCase(unittest.TestCase):
         self.assertTrue(self.is_sorted('Year'))
 
     def test_b_sort_by_ajbnum(self):
-        '''Test the sort_by routine in ajbentry.py.
+        '''Test the sort_by routine by AJB num in ajbentry.py.
 
         '''
 
-        self.bookfile.sort_by('Year')
-        self.assertFalse(self.is_sorted('Num'))
-        self.bookfile.sort_by('Num')
-        self.assertTrue(self.is_sorted('Num'))
+        self.bookfile.sort_by('AJB/AAA Num')
+        self.assertTrue(self.is_sorted('AJB/AAA Num'))
 
     def test_c_sort_by_author(self):
-        ''' Test the sort_by routine in ajbentry.py
+        ''' Test the sort_by routine by Author in ajbentry.py
 
         '''
 
-        self.bookfile.sort_by('Author')
-        #for e in self.bookfile:
-        #    if e['Authors']:
-        #        hname = e['Authors'][0]
-        #        namestr = hname.last + ', ' + hname.first + ' ' + hname.middle
-        #        print(f'{namestr}, {e["Title"]}')
-        #    else:
-        #        print(f'         {e["Title"]}')
+        self.assertFalse(self.is_sorted('Author/Editor'))
+        self.bookfile.sort_by('Author/Editor')
+        self.assertTrue(self.is_sorted('Author/Editor'))
